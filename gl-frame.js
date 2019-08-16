5d6d9edcf5644e288e1761fc470e43225e4d9be8
timothy-tianmu <tianmu_19970924@163.com>
2019-08-16 13:08:17 +0800

    customize colors

--- gl-frame.js
+++ gl-frame.js
@@ -5,29 +5,39 @@ if(!gl) {
   throw new Error("Do not support webgl")
 }
 
-var vertexShaderSource = `
-  attribute vec2 a_position;
+var getVertexShaderSource = function() {
+  return `
+    attribute vec2 a_position;
+    attribute vec4 a_color;
 
-  uniform vec2 u_resolution;
+    uniform vec2 u_resolution;
 
-  void main() {
-    vec2 zeroToOne = a_position / u_resolution;
+    varying vec4 v_color;
 
-    vec2 zeroToTwo = zeroToOne * 2.0;
+    void main() {
+      vec2 zeroToOne = a_position / u_resolution;
 
-    vec2 clipSpace = zeroToTwo - 1.0;
+      vec2 zeroToTwo = zeroToOne * 2.0;
 
-    gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
-  }
-`
+      vec2 clipSpace = zeroToTwo - 1.0;
 
-var fragmentShaderSource = `
-  precision mediump float;
+      gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+      v_color = a_color;
+    }
+  `
+}
 
-  void main() {
-    gl_FragColor = vec4(1, 0, 0.5, 0.5);
-  }
-`
+var getFragmentShaderSource = function() {
+  return `
+    precision mediump float;
+
+    varying vec4 v_color;
+
+    void main() {
+      gl_FragColor = v_color;
+    }
+  `
+}
 
 var createShader = function(gl, type, source) {
   var shader = gl.createShader(type)
@@ -41,9 +51,6 @@ var createShader = function(gl, type, source) {
   }
   return shader
 }
-
-var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
-var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
 
 var createProgram = function(gl, vertexShader, fragmentShader) {
   var program = gl.createProgram()
@@ -59,48 +66,92 @@ var createProgram = function(gl, vertexShader, fragmentShader) {
   return program
 }
 
+var vertexShader = createShader(gl, gl.VERTEX_SHADER, getVertexShaderSource())
+var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, getFragmentShaderSource())
 var program = createProgram(gl, vertexShader, fragmentShader)
 
 var positionAttributeLocation = gl.getAttribLocation(program, "a_position")
+var colorAttributeLocation = gl.getAttribLocation(program, "a_color")
 var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution")
+
+var setGeometry = function(gl) {
+  var positions = [
+    0, -100,
+    150, 125,
+    -175, 100
+  ]
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+}
+
+var setColor = function(gl) {
+  var r1 = Math.random() * 256
+  var g1 = Math.random() * 256
+  var b1 = Math.random() * 256
+  var r2 = Math.random() * 256
+  var g2 = Math.random() * 256
+  var b2 = Math.random() * 256
+  var r3 = Math.random() * 256
+  var g3 = Math.random() * 256
+  var b3 = Math.random() * 256
+  var colors = [
+    r1, g1, b1, 255,
+    r2, g2, b2, 255,
+    r3, g3, b3, 255
+  ]
+  gl.bufferData(gl.ARRAY_BUFFER, new Uint8Array(colors), gl.STATIC_DRAW)
+}
 
 var positionBuffer = gl.createBuffer()
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+setGeometry(gl)
 
-var positions = [
-  10, 20,
-  80, 20,
-  10, 30,
-  10, 30,
-  80, 20,
-  80, 30,
-]
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
-
-gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-
-gl.clearColor(0, 0, 0, 0)
-gl.clear(gl.COLOR_BUFFER_BIT)
-
-gl.useProgram(program)
-
-gl.enableVertexAttribArray(positionAttributeLocation)
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-
-gl.uniform2f(resolutionUniformLocation, 400, 300)
-
-var size = 2
-var type = gl.FLOAT
-var normalized = false
-var stride = 0
-var offset = 0
-gl.vertexAttribPointer(positionAttributeLocation, size, type, normalized, stride, offset)
-
-var primitiveType = gl.TRIANGLES
-var offset = 0
-var count = 6
-gl.drawArrays(primitiveType, offset, count)
+var colorBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+setColor(gl)
 
 
+var drawScene = function(gl) {
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+  gl.clearColor(0, 0, 0, 0)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+  gl.useProgram(program)
+
+  // 设置顶点属性即缓冲区
+  gl.enableVertexAttribArray(positionAttributeLocation)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+
+  var size = 2
+  var type = gl.FLOAT
+  var normalized = false
+  var stride = 0
+  var offset = 0
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalized, stride, offset)
+
+  // 设置颜色属性及缓冲区
+  gl.enableVertexAttribArray(colorAttributeLocation)
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+
+  var size = 4
+  var type = gl.UNSIGNED_BYTE
+  var normalized = true
+  var stride = 0
+  var offset = 0
+  gl.vertexAttribPointer(colorAttributeLocation, size, type, normalized, stride, offset)
+
+  gl.uniform2f(resolutionUniformLocation, 400, 300)
+
+  // 绘制图形
+  var primitiveType = gl.TRIANGLES
+  var offset = 0
+  var count = 3
+  gl.drawArrays(primitiveType, offset, count)
+
+}
+
+var main =  function() {
+  drawScene(gl)
+}
+
+main()
 
 
