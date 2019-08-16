b3b0e71fbe013a7c45ff28a76d552ec38a72ed8a
timothy-tianmu <tianmu_19970924@163.com>
2019-08-16 23:48:32 +0800

    make translation in GPU

--- demo_01/main.js
+++ demo_01/main.js
@@ -1,90 +1,154 @@
-var canvas = document.getElementById("gl-canvas")
-var gl = canvas.getContext("webgl")
+(function() {
+  var canvas = document.getElementById("gl-canvas")
+  var gl = canvas.getContext("webgl")
 
-if(!gl) {
-  throw new Error("Do not support webgl")
-}
+  if(!gl) {
+    throw new Error("Do not support webgl")
+  }
 
-var getVertexShaderSource = function() {
-  return `
-    attribute vec2 a_position;
+  var getVertexShaderSource = function() {
+    return `
+      attribute vec2 a_position;
 
-    uniform vec2 u_resolution;
+      uniform vec2 u_resolution;
+      uniform vec2 u_translation;
 
-    void main() {
-      vec2 zeroToOne = a_position / u_resolution;
+      void main() {
 
-      vec2 zeroToTwo = zeroToOne * 2.0;
+        vec2 position = a_position + u_translation;
 
-      vec2 clipSpace = zeroToTwo - 1.0;
+        vec2 zeroToOne = position / u_resolution;
 
-      gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
-    }
-  `
-}
+        vec2 zeroToTwo = zeroToOne * 2.0;
 
-var getFragmentShaderSource = function() {
-  return `
-    precision mediump float;
+        vec2 clipSpace = zeroToTwo - 1.0;
 
-    uniform vec4 u_color;
+        gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+      }
+    `
+  }
 
-    void main() {
-      gl_FragColor = u_color;
-    }
-  `
-}
+  var getFragmentShaderSource = function() {
+    return `
+      precision mediump float;
 
-var program = glUtils.initProgram(gl, getVertexShaderSource(), getFragmentShaderSource())
+      uniform vec4 u_color;
 
-var positionAttributeLocation = gl.getAttribLocation(program, "a_position")
-var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution")
-var colorUniformLocation = gl.getUniformLocation(program, "u_color")
+      void main() {
+        gl_FragColor = u_color;
+      }
+    `
+  }
 
-var positionBuffer = gl.createBuffer()
+  var program = glUtils.initProgram(gl, getVertexShaderSource(), getFragmentShaderSource())
 
-var setGeometry = function(gl) {
-  var positions = [
-    0, -100,
-    150, 125,
-    -175, 100
-  ]
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
-}
+  // 从着色程序中获取属性以及全局变量在内存中的位置，以便于对它们赋值
+  var positionAttributeLocation = gl.getAttribLocation(program, "a_position")
+  var translationUniformLocation = gl.getUniformLocation(program, "u_translation")
+  var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution")
+  var colorUniformLocation = gl.getUniformLocation(program, "u_color")
 
-var drawScene = function(gl) {
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
-  gl.clearColor(0, 0, 0, 0)
-  gl.clear(gl.COLOR_BUFFER_BIT)
-  gl.useProgram(program)
+  // 创建存储坐标点的缓冲区,之后如果需要使用,使用gl.bindBuffer绑定即可
+  var positionBuffer = gl.createBuffer()
 
-  gl.uniform2f(resolutionUniformLocation, 400, 300)
-  gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1)
-  // 设置顶点属性即缓冲区
-  gl.enableVertexAttribArray(positionAttributeLocation)
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+  var setRectangle = function(gl, x, y, w, h) {
+    var x1 = x;
+    var x2 = x + w;
+    var y1 = y;
+    var y2 = y + h;
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            x1, y1,
+            x2, y1,
+            x1, y2,
+            x1, y2,
+            x2, y1,
+            x2, y2,
+        ]),
+        gl.STATIC_DRAW);
+  }
 
-  var size = 2
-  var type = gl.FLOAT
-  var normalized = false
-  var stride = 0
-  var offset = 0
-  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalized, stride, offset)
+  var setGeometry = function(gl) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+    gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array([
+          // 左竖
+          0, 0,
+          30, 0,
+          0, 150,
+          0, 150,
+          30, 0,
+          30, 150,
 
-  // 绘制图形
-  var primitiveType = gl.TRIANGLES
-  var offset = 0
-  var count = 3
-  gl.drawArrays(primitiveType, offset, count)
+          // 上横
+          30, 0,
+          100, 0,
+          30, 30,
+          30, 30,
+          100, 0,
+          100, 30,
 
-}
+          // 中横
+          30, 60,
+          67, 60,
+          30, 90,
+          30, 90,
+          67, 60,
+          67, 90,
+      ]),
+      gl.STATIC_DRAW
+    )
 
-var main =  function() {
-  setGeometry(gl)
-  drawScene(gl)
-}
 
-main()
+  }
+
+  var drawScene = function(gl, translation) {
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+    gl.clearColor(0, 0, 0, 0)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.useProgram(program)
+
+    // 设置所有全局变量
+    gl.uniform2f(resolutionUniformLocation, 400, 300)
+    gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1)
+
+    gl.uniform2f(translationUniformLocation, translation[0], translation[1])
+
+    // 设置顶点属性及缓冲区
+    gl.enableVertexAttribArray(positionAttributeLocation)
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+
+    // 告诉属性如何读取缓冲区中的数据
+    var size = 2
+    var type = gl.FLOAT
+    var normalized = false
+    var stride = 0
+    var offset = 0
+    gl.vertexAttribPointer(positionAttributeLocation, size, type, normalized, stride, offset)
+
+    // 绘制图形
+    var primitiveType = gl.TRIANGLES
+    var offset = 0
+    var count = 6
+    gl.drawArrays(primitiveType, offset, count)
+
+  }
+
+  var main =  function() {
+    setGeometry(gl)
+    var translation = [0, 0]
+    setInterval(function() {
+      drawScene(gl, translation)
+      translation[0] += 5
+      translation[1] += 3
+    }, 30)
+  }
+
+  main()
 
 
+
+}())
