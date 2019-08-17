3ee6f86219fb9dfed1fbb17fce4097156af2ce4c
timothy-tianmu <tianmu_19970924@163.com>
2019-08-17 15:25:08 +0800

    store matrix by row in javascript,and modify the implementation of transformations

--- demo_03/main.js
+++ demo_03/main.js
@@ -42,9 +42,9 @@
   var m3 = {
     translation: function(tx, ty) {
       return [
-        1, 0, 0,
-        0, 1, 0,
-        tx, ty, 1,
+        1, 0, tx,
+        0, 1, ty,
+        0, 0, 1,
       ];
     },
 
@@ -63,6 +63,14 @@
         sx, 0, 0,
         0, sy, 0,
         0, 0, 1,
+      ];
+    },
+    projection: function(width, height) {
+      // 注意：这个矩阵翻转了 Y 轴，所以 0 在上方
+      return [
+        2 / width, 0, -1,
+        0, -2 / height, 1,
+        0, 0, 1
       ];
     },
 
@@ -86,25 +94,33 @@
       var b21 = b[2 * 3 + 1];
       var b22 = b[2 * 3 + 2];
       return [
-        b00 * a00 + b01 * a10 + b02 * a20,
-        b00 * a01 + b01 * a11 + b02 * a21,
-        b00 * a02 + b01 * a12 + b02 * a22,
-        b10 * a00 + b11 * a10 + b12 * a20,
-        b10 * a01 + b11 * a11 + b12 * a21,
-        b10 * a02 + b11 * a12 + b12 * a22,
-        b20 * a00 + b21 * a10 + b22 * a20,
-        b20 * a01 + b21 * a11 + b22 * a21,
-        b20 * a02 + b21 * a12 + b22 * a22,
+        a00 * b00 + a01 * b10 + a02 * b20,
+        a00 * b01 + a01 * b11 + a02 * b21,
+        a00 * b02 + a01 * b12 + a02 * b22,
+        a10 * b00 + a11 * b10 + a12 * b20,
+        a10 * b01 + a11 * b11 + a12 * b21,
+        a10 * b02 + a11 * b12 + a12 * b22,
+        a20 * b00 + a21 * b10 + a22 * b20,
+        a20 * b01 + a21 * b11 + a22 * b21,
+        a20 * b02 + a21 * b12 + a22 * b22,
       ];
     },
-    projection: function(width, height) {
-      // 注意：这个矩阵翻转了 Y 轴，所以 0 在上方
+    transpose: function(a) {
+      var a00 = a[0 * 3 + 0];
+      var a01 = a[0 * 3 + 1];
+      var a02 = a[0 * 3 + 2];
+      var a10 = a[1 * 3 + 0];
+      var a11 = a[1 * 3 + 1];
+      var a12 = a[1 * 3 + 2];
+      var a20 = a[2 * 3 + 0];
+      var a21 = a[2 * 3 + 1];
+      var a22 = a[2 * 3 + 2];
       return [
-        2 / width, 0, 0,
-        0, -2 / height, 0,
-        -1, 1, 1
-      ];
-    },
+        a00, a10, a20,
+        a01, a11, a21,
+        a02, a12, a22
+      ]
+    }
 
   }
 
@@ -172,18 +188,18 @@
     gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1)
 
     // Compute the matrices
-    var translationMatrix = m3.translation(translation[0], translation[1]);
-    var rotationMatrix = m3.rotation(angleInRadians);
     var scaleMatrix = m3.scaling(scale[0], scale[1]);
-
+    var rotationMatrix = m3.rotation(angleInRadians);
+    var translationMatrix = m3.translation(translation[0], translation[1]);
     var projectionMatrix = m3.projection(
-        gl.canvas.clientWidth, gl.canvas.clientHeight);
- 
- 
+        gl.canvas.width, gl.canvas.height);
+
     // 矩阵相乘
-    var matrix = m3.multiply(projectionMatrix, translationMatrix);
-    matrix = m3.multiply(matrix, rotationMatrix);
-    matrix = m3.multiply(matrix, scaleMatrix);
+    var matrix = scaleMatrix
+    matrix = m3.multiply(rotationMatrix, matrix);
+    matrix = m3.multiply(translationMatrix, matrix);
+    matrix = m3.multiply(projectionMatrix, matrix);
+    matrix = m3.transpose(matrix)
 
     // Set the matrix.
     gl.uniformMatrix3fv(matrixUniformLocation, false, matrix);
